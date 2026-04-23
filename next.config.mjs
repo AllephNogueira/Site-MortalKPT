91a59b7cf1a800ef64be8c3adcf30e51915530aa
Alleph Nogueira <allephn@hotmail.com.br>
2026-04-23 14:29:41 -0300

    Gerado a pasta OUT onde tem HTML/CSS/JS

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',   // 🔑 esta linha faz o Next gerar a pasta "out"
+
   typescript: {
     ignoreBuildErrors: true,
   },
